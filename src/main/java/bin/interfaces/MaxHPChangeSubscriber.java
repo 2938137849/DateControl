efd9cecfbc08e1346fea10f6aabdba006e0309d7
ccr <37367930+2938137849@users.noreply.github.com>
2019-10-30 10:46:41 +0800

    FunctionalInterface

--- src/main/java/bin/interfaces/MaxHPChangeSubscriber.java
+++ src/main/java/bin/interfaces/MaxHPChangeSubscriber.java
@@ -4,6 +4,7 @@ package bin.interfaces;
  * @author bin
  * @version 1.0.0
  */
+@FunctionalInterface
 public interface MaxHPChangeSubscriber extends basemod.interfaces.MaxHPChangeSubscriber {
   @Override
   int receiveMaxHPChange(int amount);
